4cb6468709dd851bd5a722303df21b5852c2681c
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-08 03:54:29 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end 
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(*) FROM characters INNER JOIN characters ON character_books.character_id = characters.id GROUP BY characters.name ORDER BY COUNT() "
+  "SELECT characters.name, COUNT(*) FROM characters INNER JOIN characters_books ON character_books.character_id = characters.id GROUP BY characters.name ORDER BY COUNT() "
 end
